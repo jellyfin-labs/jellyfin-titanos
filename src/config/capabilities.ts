b01f3f5fac537edc4cd466b582fffdbacedd1be8
Niels van Velzen <git@ndat.nl>
2026-04-15 21:28:34 +0200

    Add comment to DeviceCapabilities interface

--- src/config/capabilities.ts
+++ src/config/capabilities.ts
@@ -1,15 +1,20 @@
 import type { TitanSDK } from '@titan-os/sdk';
 
+/**
+ * Capabilities used by the profileBuilder in jellyfin-web. This is an **incomplete** definition that only includes the options relevant for us.
+ */
 export interface DeviceCapabilities {
-  supportsHdr10: boolean;
-  supportsDolbyVision: boolean;
+	supportsHdr10: boolean;
+	supportsDolbyVision: boolean;
 }
 
-export async function getDeviceCapabilities(titanSDK: TitanSDK): Promise<DeviceCapabilities> {
-  const nativeInfo = await titanSDK.deviceInfo.getDeviceInfo();
+export async function getDeviceCapabilities(
+	titanSDK: TitanSDK,
+): Promise<DeviceCapabilities> {
+	const nativeInfo = await titanSDK.deviceInfo.getDeviceInfo();
 
-  return {
-    supportsHdr10: !!nativeInfo.Capability.supportHDR_HDR10,
-    supportsDolbyVision: !!nativeInfo.Capability.supportHDR_DV,
-  };
+	return {
+		supportsHdr10: !!nativeInfo.Capability.supportHDR_HDR10,
+		supportsDolbyVision: !!nativeInfo.Capability.supportHDR_DV,
+	};
 }
